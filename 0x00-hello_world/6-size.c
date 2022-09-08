c82425f859dc500317d5b8d584b2cae9f5de4860
niiayiteyrichter <waltervanrichter@gmail.com>
2022-09-08 15:17:43 -0700

    s;lks;kj

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,6 @@ printf("Size of a char: %u byte(s)\n", sizeof(char));
 printf("Size of an int: %u byte(s)\n", sizeof(int));
 printf("Size of a long int: %u byte(s)\n", sizeof(long));
 printf("Size of a long long int: %u byte(s)\n", sizeof(long long));
-printf("Size of a float: %u byte(s)", sizeof(float));
+printf("Size of a float: %u byte(s)\n", sizeof(float));
 return (0);
 }
