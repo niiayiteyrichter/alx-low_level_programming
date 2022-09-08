cc0a28f483307533e8eb4fbea217a20529cfd345
niiayiteyrichter <waltervanrichter@gmail.com>
2022-09-08 15:12:39 -0700

    betty cute

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,6 @@ printf("Size of a char: %u\n", sizeof(char));
 printf("Size of an int: %u\n", sizeof(int));
 printf("Size of a long int: %u\n", sizeof(long));
 printf("Size of a long long int: %u\n", sizeof(long long));
-printf("Size of a float: %u\n", sizeof(float));
+printf("Size of a float: %u", sizeof(float));
 return (0);
 }
