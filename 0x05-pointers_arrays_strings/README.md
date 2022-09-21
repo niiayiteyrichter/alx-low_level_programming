f10b99cedfa57ec0f4c1f01173de465dd66b29b6
niiayiteyrichter <waltervanrichter@gmail.com>
2022-09-20 21:58:57 -0700

    pointers and stuff

---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+writing up some pointer stuff
---
+++ 0x05-pointers_arrays_strings/head.c
@@ -0,0 +1,8 @@
+int _atoi(char *s);void puts2(char *str);
+$ #ifndef _HOLBERTON_H_
+bash: syntax error near unexpected token `('
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
+					    
